49c724c3e774aac05995288359c8ad16761c5736
Danel Sibaev <34159440+d4n0n-myself@users.noreply.github.com>
2020-03-02 09:31:41 +0300

    size and pages

--- heap-toast-fillfactor/secondTask.sql
+++ heap-toast-fillfactor/secondTask.sql
@@ -49,3 +49,18 @@ select updateVarcharValue('fillfactor75');
 select updateVarcharValue('fillfactor90');
 select updateVarcharValue('fillfactor100');
 --#endregion
+
+--#region show size of tables
+SELECT pg_size_pretty(pg_relation_size(c.OID)) AS "Size of %s" FROM pg_class c where relname = 'fillfactor50';
+SELECT pg_size_pretty(pg_relation_size(c.OID)) AS "Size of %s" FROM pg_class c where relname = 'fillfactor75';
+SELECT pg_size_pretty(pg_relation_size(c.OID)) AS "Size of %s" FROM pg_class c where relname = 'fillfactor90';
+SELECT pg_size_pretty(pg_relation_size(c.OID)) AS "Size of %s" FROM pg_class c where relname = 'fillfactor100';
+--#endregion
+
+--#region get full pages
+CREATE EXTENSION pageinspect;
+SELECT get_raw_page('fillfactor50', 0);
+SELECT get_raw_page('fillfactor75', 0);
+SELECT get_raw_page('fillfactor90', 0);
+SELECT get_raw_page('fillfactor100', 0);
+--#endregion
